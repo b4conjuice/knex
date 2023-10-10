6a077fab1b057004093c02b4a74babff6e6eb043
Daniel Lopez <dlopez807@gmail.com>
2023-10-09 21:28:59 -0700

    add ability to select words

---
+++ src/components/button.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import classnames from 'classnames'
+
+// based on: https://www.joshwcomeau.com/animation/3d-button/
+export default function Button({
+  className,
+  onClick,
+  href,
+  type,
+  disabled,
+  textSizeClassName = 'text-lg',
+  children,
+}: {
+  className?: string
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
+  href?: string
+  type?: 'button' | 'submit' | 'reset' | undefined
+  disabled?: boolean
+  textSizeClassName?: string
+  children?: React.ReactNode
+}) {
+  if (href) {
+    const anchorProps = {
+      href,
+      target: '_blank',
+      rel: 'noopener noreferrer',
+    }
+    return (
+      <a
+        className={classnames(
+          textSizeClassName,
+          'bg-cb-dark-blue group w-full cursor-pointer rounded-lg border-none text-center',
+          className
+        )}
+        {...anchorProps}
+      >
+        <span className='block translate-y-[-4px] transform rounded-lg bg-[#5a3e84] p-3 duration-[600ms] ease-[cubic-bezier(.3,.7,.4,1)] hover:ease-[cubic-bezier(.3,.7,.4,1.5)] group-hover:translate-y-[-6px] group-hover:duration-[250ms] group-active:translate-y-[-2px] group-active:duration-[34ms]'>
+          {children}
+        </span>
+      </a>
+    )
+  }
+  const buttonProps = {
+    onClick,
+    disabled: disabled ?? false,
+    type: type ?? 'button',
+  }
+  return (
+    <button
+      className={classnames(
+        textSizeClassName,
+        'bg-cb-dark-blue group w-full cursor-pointer rounded-lg border-none text-center',
+        className
+      )}
+      {...buttonProps}
+    >
+      <span className='block translate-y-[-4px] transform rounded-lg bg-[#5a3e84] p-3 duration-[600ms] ease-[cubic-bezier(.3,.7,.4,1)] hover:ease-[cubic-bezier(.3,.7,.4,1.5)] group-hover:translate-y-[-6px] group-hover:duration-[250ms] group-active:translate-y-[-2px] group-active:duration-[34ms]'>
+        {children}
+      </span>
+    </button>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 import { useState } from 'react'
 import { Main, Title } from '@bacondotbuild/ui'
-import { Cog6ToothIcon } from '@heroicons/react/24/solid'
+import {
+  ArrowsUpDownIcon,
+  CheckCircleIcon,
+  Cog6ToothIcon,
+} from '@heroicons/react/24/solid'
+import classNames from 'classnames'
 import {
   DndContext,
   closestCenter,
@@ -21,6 +26,7 @@ import { CSS } from '@dnd-kit/utilities'
 
 import Layout from '@/components/layout'
 import Modal from '@/components/modal'
+import Button from '@/components/button'
 import useLocalStorage from '@/lib/useLocalStorage'
 
 const defaultWords = [
@@ -64,12 +70,29 @@ function SortableItem({
   )
 }
 
+const MAX_SELECTED_WORDS = 4
+
 export default function Home() {
   const [savedWords, setSavedWords] = useLocalStorage<string[]>(
     'knex-savedWords',
     [...defaultWords]
   )
+  const [selectedWords, setSelectedWords] = useLocalStorage<string[]>(
+    'knex-selectedWords',
+    []
+  )
+  const selectWord = (selectedWord: string) => {
+    const selectedWordIndex = selectedWords.findIndex(s => s === selectedWord)
+    if (selectedWordIndex > -1) {
+      const newSelectedWords = [...selectedWords]
+      newSelectedWords.splice(selectedWordIndex, 1)
+      setSelectedWords(newSelectedWords)
+    } else if (selectedWords?.length < MAX_SELECTED_WORDS) {
+      setSelectedWords([...selectedWords, selectedWord])
+    }
+  }
   const [showModal, setShowModal] = useState(false)
+  const [isSortMode, setIsSortMode] = useState(false)
   const items = savedWords || []
 
   const sensors = useSensors(
@@ -117,28 +140,104 @@ export default function Home() {
             onDragEnd={handleDragEnd}
           >
             <SortableContext items={items} strategy={rectSortingStrategy}>
-              <ul className='grid grid-cols-4 gap-4'>
-                {items
-                  .map(item => ({ id: item, item }))
-                  .map((item, index) => (
-                    <SortableItem key={item.id} id={item.id}>
-                      <div
-                        key={index}
-                        className='max-w-xs flex-col gap-4 rounded-xl bg-white/10 px-2 py-4 text-center text-white hover:bg-white/20'
-                      >
-                        {item.item}
-                      </div>
-                    </SortableItem>
+              {isSortMode ? (
+                <ul className='grid w-full grid-cols-4 gap-4'>
+                  {items
+                    .map(item => ({ id: item, item }))
+                    .map((item, index) => (
+                      <SortableItem key={item.id} id={item.id}>
+                        <div
+                          key={index}
+                          className='flex-col gap-4 rounded-xl border border-white/10 bg-white/10 px-2 py-4 text-center text-white'
+                        >
+                          {item.item}
+                        </div>
+                      </SortableItem>
+                    ))}
+                </ul>
+              ) : (
+                <ul className='grid w-full grid-cols-4 gap-4'>
+                  {items.map((item, index) => (
+                    <button
+                      key={index}
+                      className={classNames(
+                        selectedWords.includes(item)
+                          ? 'border-cb-yellow'
+                          : 'border-white/10',
+                        'gap-4 rounded-xl border bg-white/10 px-2 py-4 text-center text-white'
+                      )}
+                      type='button'
+                      onClick={() => {
+                        selectWord(item)
+                      }}
+                    >
+                      {item}
+                    </button>
                   ))}
-              </ul>
+                </ul>
+              )}
             </SortableContext>
           </DndContext>
+          <label
+            htmlFor='checked'
+            className='inline-flex w-full cursor-pointer items-center justify-center space-x-3'
+          >
+            <span className={isSortMode ? 'text-cb-yellow' : ''}>
+              <ArrowsUpDownIcon className='h-6 w-6' />
+            </span>
+            <span className='relative'>
+              <span className='block h-6 w-10 rounded-full bg-gray-400 shadow-inner' />
+              <span
+                className={`focus-within:shadow-outline bg-cb-yellow absolute inset-y-0 left-0 ml-1 mt-1 block h-4 w-4 rounded-full shadow transition-transform duration-300 ease-in-out ${
+                  !isSortMode ? 'translate-x-full transform' : ''
+                }`}
+              >
+                <input
+                  id='checked'
+                  type='checkbox'
+                  checked
+                  onChange={() => setIsSortMode(!isSortMode)}
+                  className='absolute h-0 w-0 opacity-0'
+                />
+              </span>
+            </span>
+            <span className={!isSortMode ? 'text-cb-yellow' : ''}>
+              <CheckCircleIcon className='h-6 w-6' />
+            </span>
+          </label>
+          {selectedWords?.length > 0 && (
+            <ul className='grid w-full grid-cols-4 gap-4'>
+              {selectedWords.map((word, index) => (
+                <div
+                  key={index}
+                  className='max-w-xs flex-col gap-4 rounded-xl bg-white/10 px-2 py-4 text-center text-white'
+                >
+                  {word}
+                </div>
+              ))}
+            </ul>
+          )}
         </div>
-        {/* <div className='flex justify-evenly gap-4'>
+        <div className='flex justify-evenly gap-4'>
           <Button>shuffle</Button>
-          <Button>deselect all</Button>
-          <Button>submit</Button>
-        </div> */}
+          <Button
+            type='button'
+            onClick={() => {
+              setSelectedWords([])
+            }}
+            disabled={selectedWords?.length === 0}
+            className='disabled:pointer-events-none disabled:opacity-25'
+            textSizeClassName='text-sm'
+          >
+            deselect all
+          </Button>
+          <Button
+            disabled={selectedWords?.length !== MAX_SELECTED_WORDS}
+            className='disabled:pointer-events-none disabled:opacity-25'
+          >
+            submit
+          </Button>
+        </div>
         <Modal isOpen={showModal} setIsOpen={setShowModal} title='settings'>
           <textarea
             className='bg-cobalt w-full flex-grow p-4'
